bfdfb9b420e405647fc44d48aa14949d18a52a92
Anton Kolb <anton.kolb@gmail.com>
2024-04-26 16:35:04 +0300

    Create Client entity class + repository

---
+++ src/main/kotlin/com/kotsen/kotlinusermanager/entity/Client.kt
@@ -0,0 +1,15 @@
+package com.kotsen.kotlinusermanager.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity(name = "task")
+data class Client(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Long?,
+        var name: String,
+        var description: String,
+        var done: Boolean)
