428b97d03ad68d10cfd5fac2ade51b1ee4c5e874
Anton Kolb <anton.kolb@gmail.com>
2024-04-30 11:46:21 +0300

    Extended test coverage

--- src/test/kotlin/com/andersen/usermanager/data/TestDataFactory.kt
+++ src/test/kotlin/com/andersen/usermanager/data/TestDataFactory.kt
@@ -40,4 +40,32 @@ object TestDataFactory {
         )
     }
 
+    fun createClient_1(): Client {
+        return Client(
+            id = 2L,
+            firstName = "Jason",
+            lastName = "Dawson",
+            email = "johndoe@example.com",
+            gender = Gender.MALE,
+            job = "Software Engineer",
+            position = "Senior Developer"
+        )
+    }
+
+    fun createListOfClients(count: Int): List<Client> {
+        val clients = mutableListOf<Client>()
+        for (i in 1..count) {
+            val client = Client(
+                id = i.toLong(),
+                firstName = "ClientFirstName$i",
+                lastName = "ClientLastName$i",
+                email = "client$i@example.com",
+                gender = if (i % 2 == 0) Gender.MALE else Gender.FEMALE,
+                job = "Job$i",
+                position = "Position$i"
+            )
+            clients.add(client)
+        }
+        return clients
+    }
 }
--- src/test/kotlin/com/andersen/usermanager/unit/ClientServiceImplTest.kt
+++ src/test/kotlin/com/andersen/usermanager/unit/ClientServiceImplTest.kt
@@ -9,14 +9,19 @@ import com.andersen.usermanager.service.impl.ClientServiceImpl
 import com.andersen.usermanager.service.impl.GenderApiServiceImpl
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mock
+import org.mockito.Mockito.never
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
 import java.util.Optional
 
 class ClientServiceImplTest {
@@ -36,8 +41,55 @@ class ClientServiceImplTest {
     }
 
     @Test
-    fun createClient_WhenEmailIsAvailable_ShouldCreateClient() {
+    fun createClient_GenderNotSet_ShouldCreateClient() {
+        val clientRequestDTO = TestDataFactory.createGenderNotDefinedClientRequestDTO()
+        val client = TestDataFactory.createClient()
+
+        `when`(clientRepository.findByEmail(clientRequestDTO.email)).thenReturn(null)
+        `when`(genderApiServiceImpl.defineClientGender(clientRequestDTO.firstName)).thenReturn("MALE")
+        `when`(clientRepository.save(any(Client::class.java))).thenAnswer { invocation ->
+            val savedClient = invocation.getArgument<Client>(0)
+            savedClient.id = 1L
+            savedClient
+        }
+
+        val clientResponseDTO = clientServiceImpl.createClient(clientRequestDTO)
+
+        assertEquals(client.id, clientResponseDTO.id)
+        assertEquals(client.firstName, clientResponseDTO.firstName)
+        assertEquals(client.lastName, clientResponseDTO.lastName)
+        assertEquals(client.email, clientResponseDTO.email)
+        assertEquals(client.gender, clientResponseDTO.gender)
+        assertEquals(client.job, clientResponseDTO.job)
+        assertEquals(client.position, clientResponseDTO.position)
+    }
+
+    @Test
+    fun createClient_GenderSet_ShouldCreateClient() {
         val clientRequestDTO = TestDataFactory.createGenderDefinedClientRequestDTO()
+        val client = TestDataFactory.createClient()
+
+        `when`(clientRepository.findByEmail(clientRequestDTO.email)).thenReturn(null)
+        `when`(clientRepository.save(any(Client::class.java))).thenAnswer { invocation ->
+            val savedClient = invocation.getArgument<Client>(0)
+            savedClient.id = 1L
+            savedClient
+        }
+
+        val clientResponseDTO = clientServiceImpl.createClient(clientRequestDTO)
+
+        assertEquals(client.id, clientResponseDTO.id)
+        assertEquals(client.firstName, clientResponseDTO.firstName)
+        assertEquals(client.lastName, clientResponseDTO.lastName)
+        assertEquals(client.email, clientResponseDTO.email)
+        assertEquals(client.gender, clientResponseDTO.gender)
+        assertEquals(client.job, clientResponseDTO.job)
+        assertEquals(client.position, clientResponseDTO.position)
+    }
+
+    @Test
+    fun createClient_WhenEmailIsAvailable_ShouldCreateClient() {
+        val clientRequestDTO = TestDataFactory.createGenderNotDefinedClientRequestDTO()
         val client = TestDataFactory.createClient()
 
         `when`(clientRepository.findByEmail(clientRequestDTO.email)).thenReturn(null)
@@ -109,6 +161,155 @@ class ClientServiceImplTest {
         assertThrows(ClientNotFoundException::class.java) {
             clientServiceImpl.updateClient(clientId, clientRequestDTO)
         }
+    }
+
+    @Test
+    fun updateClient_WhenClientEmailAlreadyUsed_ShouldThrowEmailAlreadyRegisteredException() {
+        val clientId = 1L
+        val clientDTO = TestDataFactory.createGenderDefinedClientRequestDTO()
+        val existingClient = TestDataFactory.createClient()
+        val existingClientByEmail = TestDataFactory.createClient_1()
+
+        `when`(clientRepository.findById(clientId)).thenReturn(Optional.of(existingClient))
+        `when`(clientRepository.findByEmail(clientDTO.email)).thenReturn(existingClientByEmail)
+
+        assertThrows<EmailAlreadyRegisteredException> {
+            clientServiceImpl.updateClient(clientId, clientDTO)
+        }
+
+        verify(clientRepository, times(1)).findById(clientId)
+        verify(clientRepository, times(1)).findByEmail(clientDTO.email)
+        verify(clientRepository, never()).save(any())
+    }
+
+    @Test
+    fun deleteClient_WhenClientExists_ShouldCallDeleteByIdOnRepository() {
+        val clientId = 1L
+
+        clientServiceImpl.deleteClient(clientId)
+
+        verify(clientRepository, times(1)).deleteById(clientId)
+    }
+
+    @Test
+    fun getClient_WhenClientExists_ShouldReturnClientResponseDTO() {
+        val clientId = 1L
+        val client = TestDataFactory.createClient()
+
+        `when`(clientRepository.findById(clientId)).thenReturn(Optional.of(client))
+
+        val clientResponseDTO = clientServiceImpl.getClient(clientId)
+
+        assertEquals(client.id, clientResponseDTO.id)
+        assertEquals(client.firstName, clientResponseDTO.firstName)
+        assertEquals(client.lastName, clientResponseDTO.lastName)
+        assertEquals(client.email, clientResponseDTO.email)
+        assertEquals(client.gender, clientResponseDTO.gender)
+        assertEquals(client.job, clientResponseDTO.job)
+        assertEquals(client.position, clientResponseDTO.position)
+
+        verify(clientRepository, times(1)).findById(clientId)
+    }
+
+    @Test
+    fun getClient_WhenClientDoesNotExist_ShouldThrowClientNotFoundException() {
+        val clientId = 1L
+
+        `when`(clientRepository.findById(clientId)).thenReturn(Optional.empty())
+
+        assertThrows<ClientNotFoundException> {
+            clientServiceImpl.getClient(clientId)
+        }
+
+        verify(clientRepository, times(1)).findById(clientId)
+    }
+
+    @Test
+    fun getAllClients_WhenClientsExist_ShouldReturnPageOfClientResponseDTO() {
+        val pageable = PageRequest.of(0, 10)
+        val clients = TestDataFactory.createListOfClients(20)
+        val clientsPage = PageImpl(clients, pageable, clients.size.toLong())
+
+        `when`(clientRepository.findAll(pageable)).thenReturn(clientsPage)
+
+        val resultPage = clientServiceImpl.getAllClients(pageable)
+
+        assertEquals(clients.size.toLong(), resultPage.totalElements)
+        assertEquals(clients.size, resultPage.content.size)
+
+        for (i in clients.indices) {
+            val expectedClient = clients[i]
+            val actualClient = resultPage.content[i]
+
+            assertEquals(expectedClient.id, actualClient.id)
+            assertEquals(expectedClient.firstName, actualClient.firstName)
+            assertEquals(expectedClient.lastName, actualClient.lastName)
+            assertEquals(expectedClient.email, actualClient.email)
+            assertEquals(expectedClient.gender, actualClient.gender)
+            assertEquals(expectedClient.job, actualClient.job)
+            assertEquals(expectedClient.position, actualClient.position)
+        }
+
+        verify(clientRepository, times(1)).findAll(pageable)
+    }
+
+    @Test
+    fun getClientsByNames_WhenClientsExist_ShouldReturnMatchingClients() {
+        val firstName = "J"
+        val lastName = "D"
+        val pageable = PageRequest.of(0, 10)
+
+        val client1 = TestDataFactory.createClient()
+        val client2 = TestDataFactory.createClient_1()
+
+        val clients = listOf(client1, client2)
+        val page = PageImpl(clients)
+
+        `when`(clientRepository.findByFirstNameAndLastName(firstName, lastName, pageable)).thenReturn(page)
+
+        val result = clientServiceImpl.getClientsByNames(firstName, lastName, pageable)
+
+        assertEquals(page.totalElements, result.totalElements)
+        assertEquals(page.number, result.number)
+        assertEquals(page.size, result.size)
+
+        assertEquals(client1.id, result.content[0].id)
+        assertEquals(client1.firstName, result.content[0].firstName)
+        assertEquals(client1.lastName, result.content[0].lastName)
+        assertEquals(client1.email, result.content[0].email)
+        assertEquals(client1.gender, result.content[0].gender)
+        assertEquals(client1.job, result.content[0].job)
+        assertEquals(client1.position, result.content[0].position)
+
+        assertEquals(client2.id, result.content[1].id)
+        assertEquals(client2.firstName, result.content[1].firstName)
+        assertEquals(client2.lastName, result.content[1].lastName)
+        assertEquals(client2.email, result.content[1].email)
+        assertEquals(client2.gender, result.content[1].gender)
+        assertEquals(client2.job, result.content[1].job)
+        assertEquals(client2.position, result.content[1].position)
+
+        verify(clientRepository, times(1)).findByFirstNameAndLastName(firstName, lastName, pageable)
+    }
+
+    @Test
+    fun getClientsByNames_WhenNoClientsExist_ShouldReturnEmptyPage() {
+        val firstName = "Nonexistent"
+        val lastName = "User"
+        val pageable = PageRequest.of(0, 10)
+
+        val page = PageImpl(emptyList<Client>())
+
+        `when`(clientRepository.findByFirstNameAndLastName(firstName, lastName, pageable)).thenReturn(page)
+
+        val result = clientServiceImpl.getClientsByNames(firstName, lastName, pageable)
+
+        assertEquals(0, result.totalElements)
+        assertEquals(0, result.number)
+        assertEquals(0, result.size)
+        assertTrue(result.content.isEmpty())
+
+        verify(clientRepository, times(1)).findByFirstNameAndLastName(firstName, lastName, pageable)
     }
 
 }
